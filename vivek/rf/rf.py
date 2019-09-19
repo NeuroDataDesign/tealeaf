fbf826167f8339c9466b1ffb1522873c2012f7ce
v715 <vgopala4@jhu.edu>
2019-09-19 01:33:41 -0400

    Fix bugs

--- vivek/rf/rf.py
+++ vivek/rf/rf.py
@@ -1,16 +1,17 @@
 from collections import namedtuple
+import random
 
 import numpy as np
 
 
 # Named tuple for groups
-Group = namedtuple('Group', 'X y')
+Group = namedtuple("Group", "X y")
 
 
 # Calcualte information criteria
 def information(y):
 
-    y_bar = np.mean(y, axis=1)
+    y_bar = np.mean(y, axis=1).reshape(-1, 1)
 
     score = 0
 
@@ -41,10 +42,10 @@ def partition(X, y, feature, condition):
 # Find the best split at a given node
 def find_best_partition(X, y, n_features):
 
-    best_score = 999
+    best_score = 10 ** 8
 
     # Choose random features
-    chosen_features = random.sample(list(range(0, len(X[0])-1)), n_features)
+    chosen_features = random.sample(list(range(0, len(X[0]) - 1)), n_features)
 
     found = False
 
@@ -54,14 +55,16 @@ def find_best_partition(X, y, n_features):
 
         for j in possible_conditions:
 
-            group_1, group_2 = partition(data, feature=i, condition=j)
+            group_1, group_2 = partition(X, y, feature=i, condition=j)
 
             if len(group_1.X) < 10 or len(group_2.X) < 10:
                 continue
 
-            score = information(y)*len(y) \
-                - information(group_1.y)*len(group_1.y) \
-                - information(group_2.y)*len(group_2.y)
+            score = (
+                information(y) * len(y)
+                - information(group_1.y) * len(group_1.y)
+                - information(group_2.y) * len(group_2.y)
+            )
 
             if score < best_score:
                 found = True
@@ -75,9 +78,9 @@ def find_best_partition(X, y, n_features):
         best_condition = 1
         best_feature = 1
         best_group_1 = Group(X, y)
-        best_group_2 = []
+        best_group_2 = Group(np.array([]), np.array([]))
 
-    return (best_group_1, best_group_2, best_condition, best_feature, best_score)
+    return best_group_1, best_group_2, best_condition, best_feature, best_score
 
 
 class RFDecisionNode:
@@ -98,7 +101,7 @@ class RFDecisionNode:
             return False
 
     def _get_prediction(self):
-        return np.mean(y, axis=1)
+        return np.mean(self.y, axis=1)
 
     # Split node and create child nodes
     def split(self):
@@ -106,16 +109,28 @@ class RFDecisionNode:
             self.terminal = True
             self.prediction = self._get_prediction()
         else:
-            (group_1, group_2, self.condition, self.feature, score) = find_best_partition(self.X, self.y, self.n_features)
+            group_1, group_2, self.condition, self.feature, score = find_best_partition(
+                self.X, self.y, self.n_features
+            )
 
             if len(group_1.X) == 0 or len(group_2.X) == 0:
                 self.terminal = True
                 self.prediction = self._get_prediction()
             else:
                 self.left = RFDecisionNode(
-                    group_1.X, group_1.y, self.max_depth, self.n_features, self.depth + 1)
+                    group_1.X,
+                    group_1.y,
+                    self.max_depth,
+                    self.n_features,
+                    self.depth + 1,
+                )
                 self.right = RFDecisionNode(
-                    group_2.X, group_2.y, self.max_depth, self.n_features, self.depth + 1)
+                    group_2.X,
+                    group_2.y,
+                    self.max_depth,
+                    self.n_features,
+                    self.depth + 1,
+                )
                 self.left.split()
                 self.right.split()
 
