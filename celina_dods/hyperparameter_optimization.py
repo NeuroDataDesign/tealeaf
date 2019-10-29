f29e6f4b470e1100507957f4b4ba514634d9b092
Celina <cshih14@jhu.edu>
2019-10-28 22:25:55 -0400

    added hyperparamter_optimization.py

---
+++ celina_dods/hyperparameter_optimization.py
@@ -0,0 +1,19 @@
+import numpy as np
+from sklearn.model_selection import RandomizedSearchCV
+import openml
+from rerf.rerfClassifier import rerfClassifier
+from sklearn.model_selection import KFold
+
+def hyperparameter_optimization(X,y,*argv):
+    
+    clf_best_params = {}
+    for clf, params in argv:
+
+        # run randomized search     
+        n_iter_search = 10
+        random_search = RandomizedSearchCV(clf, param_distributions=param_dist,
+                                           n_iter=n_iter_search, cv=10, iid=False)
+        random_search.fit(X, y)       
+        clf_best_params[clf] = random_search.best_params_  
+    return clf_best_params
+
