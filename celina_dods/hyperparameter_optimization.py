077278e224fc64fc9c864327292322df25554aff
v715 <vgopala4@jhu.edu>
2019-10-28 22:56:03 -0400

    Add comments

--- celina_dods/hyperparameter_optimization.py
+++ celina_dods/hyperparameter_optimization.py
@@ -27,13 +27,17 @@ def hyperparameter_optimization(X, y, *argv):
 
     clf_best_params = {}
 
+    # Iterate over all (classifier, hyperparameters) pairs
     for clf, params in argv:
 
-        # run randomized search
+        # Run randomized search
         n_iter_search = 10
         random_search = RandomizedSearchCV(
             clf, param_distributions=params, n_iter=n_iter_search, cv=10, iid=False
         )
         random_search.fit(X, y)
+
+        # Save results
         clf_best_params[clf] = random_search.best_params_
+
     return clf_best_params
