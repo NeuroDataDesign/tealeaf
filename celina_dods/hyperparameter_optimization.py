8a08288db0390c9022cf1f951500853f7eb76ad1
v715 <vgopala4@jhu.edu>
2019-10-28 22:48:41 -0400

    Autoformat with Black

--- celina_dods/hyperparameter_optimization.py
+++ celina_dods/hyperparameter_optimization.py
@@ -1,15 +1,16 @@
 from sklearn.model_selection import RandomizedSearchCV
 
 
-def hyperparameter_optimization(X,y,*argv):
-    
+def hyperparameter_optimization(X, y, *argv):
+
     clf_best_params = {}
     for clf, params in argv:
 
-        # run randomized search     
+        # run randomized search
         n_iter_search = 10
-        random_search = RandomizedSearchCV(clf, param_distributions=param_dist,
-                                           n_iter=n_iter_search, cv=10, iid=False)
-        random_search.fit(X, y)       
-        clf_best_params[clf] = random_search.best_params_  
+        random_search = RandomizedSearchCV(
+            clf, param_distributions=param_dist, n_iter=n_iter_search, cv=10, iid=False
+        )
+        random_search.fit(X, y)
+        clf_best_params[clf] = random_search.best_params_
     return clf_best_params
