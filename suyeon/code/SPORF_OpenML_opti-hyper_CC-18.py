5ff21c6fbbfc4e2a42a2b68d1e7c49468be4688c
Suyeon Ju <sju7@jhu.edu>
2019-12-08 20:08:41 -0500

    implement celina's example into my code for first dataset

--- suyeon/code/SPORF_OpenML_opti-hyper_CC-18.py
+++ suyeon/code/SPORF_OpenML_opti-hyper_CC-18.py
@@ -1,9 +1,21 @@
-from sklearn.ensemble import RandomForestClassifier
-from rerf.rerfClassifier import rerfClassifier
-from sklearn.model_selection import train_test_split
+# OpenML CC-18
+
+## Load in datasets
+
 import openml
-from sklearn import metrics
+import sklearn
+from rerf.rerfClassifier import rerfClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from datetime import datetime
 import numpy as np
+import pandas as pd
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV
+from scipy.stats import randint as sp_randint
+import math
+from math import log
 
 from warnings import simplefilter
 simplefilter(action='ignore', category=FutureWarning)
@@ -37,37 +49,56 @@ dict = {rerfClassifier(feature_combinations=1.5, image_height=None, image_width=
 keys, values = zip(*dict.items())
 
 # get some data
-task_id = 146821 #car
-openml.config.apikey = 'c9ea8896542dd998ea42685f14e2bc14'
+openml.config.apikey = '204cdba18d110fd68ad24b131ea92030'
 benchmark_suite = openml.study.get_suite('OpenML-CC18')
-task = openml.tasks.get_task(task_id)
-X, y = task.get_X_and_y()
-n_features = np.shape(X)[1]
-n_samples = np.shape(X)[0]
 
-#train test split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+for task_id in benchmark_suite.tasks[0:1]:  # iterate over all tasks
+    try:
+        # f = open("SPORF_accuracies_opti-hyper_CC-18.txt","a")
 
-rerf_opti = rerfClassifier(**values[0])
-rerf_opti.fit(X_train, y_train)
-rerf_pred_opti = rerf_opti.predict(X_test)
-rerf_accuracy_opti = metrics.accuracy_score(y_test, rerf_pred_opti)
-print(rerf_accuracy_opti)
-    
-rerf_default = RandomForestClassifier()
-rerf_default.fit(X_train, y_train)
-rerf_pred_default = rerf_default.predict(X_test)
-rerf_accuracy_default = metrics.accuracy_score(y_test, rerf_pred_default)
-print(rerf_accuracy_default)
+        print(task_id)
+        print('Data set: %s: ' % (task.get_dataset().name))
 
-rf_opti = RandomForestClassifier(**values[1])
-rf_opti.fit(X_train, y_train)
-rf_pred_opti = rf_opti.predict(X_test)
-rf_accuracy_opti = metrics.accuracy_score(y_test, rf_pred_opti)
-print(rf_accuracy_opti)
-    
-rf_default = RandomForestClassifier()
-rf_default.fit(X_train, y_train)
-rf_pred_default = rf_default.predict(X_test)
-rf_accuracy_default = metrics.accuracy_score(y_test, rf_pred_default)
-print(rf_accuracy_default)
+        task = openml.tasks.get_task(task_id)
+        X, y = task.get_X_and_y()
+        n_features = np.shape(X)[1]
+        n_samples = np.shape(X)[0]
+
+        #train test split
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+        rerf_opti = rerfClassifier(**values[0])
+        rerf_opti.fit(X_train, y_train)
+        rerf_pred_opti = rerf_opti.predict(X_test)
+        rerf_accuracy_opti = accuracy_score(y_test, rerf_pred_opti)
+        print(rerf_accuracy_opti)
+            
+        rerf_default = RandomForestClassifier()
+        rerf_default.fit(X_train, y_train)
+        rerf_pred_default = rerf_default.predict(X_test)
+        rerf_accuracy_default = accuracy_score(y_test, rerf_pred_default)
+        print(rerf_accuracy_default)
+
+        rf_opti = RandomForestClassifier(**values[1])
+        rf_opti.fit(X_train, y_train)
+        rf_pred_opti = rf_opti.predict(X_test)
+        rf_accuracy_opti = accuracy_score(y_test, rf_pred_opti)
+        print(rf_accuracy_opti)
+            
+        rf_default = RandomForestClassifier()
+        rf_default.fit(X_train, y_train)
+        rf_pred_default = rf_default.predict(X_test)
+        rf_accuracy_default = accuracy_score(y_test, rf_pred_default)
+        print(rf_accuracy_default)
+        
+        print('Time: '+ str(datetime.now() - startTime))
+        
+        # f.write('%i,%s,%s,%f,%f,%f,%f,%f\n' % (task_id,task.get_dataset().name,str(datetime.now() - startTime),clf_best_params["feature_combinations"],clf_best_params["max_depth"],clf_best_params["max_features"],clf_best_params["min_samples_split"],clf_best_params["n_estimators"]))
+        # f.close()
+    except:
+        print('Error in OpenML CC-18 dataset ' + str(task_id))
+
+
+
+
+
